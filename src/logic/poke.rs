3b4cd19e793d2b312de8a0d60a8f5b4b90c2bda0
vintcessun <vintces@163.com>
2026-07-02 00:45:52 +0800

    修复poke模块，更新README

--- src/logic/poke.rs
+++ src/logic/poke.rs
@@ -1,7 +1,13 @@
+use crate::abi::message::event_notice::Notify;
 use crate::abi::{logic_import::*, message::from_str};
+use crate::logic::Notice;
 
 #[handler(msg_type=Notice)]
-pub async fn github(ctx: Context) -> Result<()> {
-    ctx.send_message_async(from_str("喵喵喵"));
+pub async fn poke(ctx: Context) -> Result<()> {
+    if let Notice::Notify(e) = ctx.get_message().as_ref()
+        && let Notify::Poke(_) = e
+    {
+        ctx.send_message(from_str("喵喵喵")).await?;
+    }
     Ok(())
 }
